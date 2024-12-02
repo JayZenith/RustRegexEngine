c1701e36e04832e235c535cb31820bf2ad160bcf
Jay Zenith <jayzenith248@gmail.com>
2024-12-01 16:40:42 -0800

    cleaned up adding to states in string.rs

--- string.rs
+++ string.rs
@@ -3,7 +3,8 @@ use std::ops::Range;
 
 type FsmIndex = usize;
 
-const FSM_COLUMN_SIZE: usize = 127;
+const FSM_COLUMN_SIZE: usize = 130;
+const FSM_NEWLINE: usize = 129;
 
 struct FsmColumn {
     ts: [FsmIndex; FSM_COLUMN_SIZE], //array of 127 usize
@@ -54,18 +55,49 @@ impl Fsm{
 fn main(){
     let mut fsm = Fsm::new();
 
-
-    
+    let events = vec!['a' as usize, 'b' as usize, 'c' as usize, FSM_NEWLINE];    
     //Failed State
     fsm.push(FsmColumn::new());
 
-    //FsmColumn 1
-    {
-        let mut col = FsmColumn::new(); //get struct with 0 init array
-        //col.fill_range('a'..'b', 1); //ascii char index with state 1 assigned 
-        col.ts['a' as usize] = fsm.cs.len() + 1;
+
+    for event in events.iter(){
+        let mut col = FsmColumn::new();
+        col.ts[*event] = fsm.cs.len() + 1;
         fsm.push(col);
     }
 
+    // //FsmColumn 1
+    // {
+    //     let mut col = FsmColumn::new(); //get struct with 0 init array
+    //     //col.fill_range('a'..'b', 1); //ascii char index with state 1 assigned 
+    //     col.ts['a' as usize] = fsm.cs.len() + 1;
+    //     fsm.push(col);
+    // }
+
+    // //FsmColumn 2
+    // {
+    //     let mut col = FsmColumn::new(); //get struct with 0 init array
+    //     //col.fill_range('a'..'b', 1); //ascii char index with state 1 assigned 
+    //     col.ts['b' as usize] = fsm.cs.len() + 1;
+    //     fsm.push(col);
+    // }
+
+    // //FsmColumn 3
+    // {
+    //     let mut col = FsmColumn::new(); //get struct with 0 init array
+    //     //col.fill_range('a'..'b', 1); //ascii char index with state 1 assigned 
+    //     col.ts['c' as usize] = fsm.cs.len() + 1;
+    //     fsm.push(col);
+    // }
+
+    // //FsmColumn 4
+    // {
+    //     let mut col = FsmColumn::new(); //get struct with 0 init array
+    //     //col.fill_range('a'..'b', 1); //ascii char index with state 1 assigned 
+    //     col.ts[FSM_NEWLINE] = fsm.cs.len() + 1;
+    //     fsm.push(col);
+    // }
+
+    
     fsm.dump();
 }
